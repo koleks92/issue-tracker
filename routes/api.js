85f14351e6d312d97eb814a18fd55be1103a352e
koleks92 <koleks92@gmail.com>
2024-07-11 20:29:25 +0200

    feat: api post

--- routes/api.js
+++ routes/api.js
@@ -5,12 +5,24 @@ module.exports = function (app) {
   app.route('/api/issues/:project')
   
     .get(function (req, res){
-      let project = req.params.project;
+      const project = req.params.project;
+      
+      res.json("Data")
       
     })
     
     .post(function (req, res){
       let project = req.params.project;
+      let data = req.body;
+
+      res.json({
+        issue_title: data.issue_title,
+        issue_text: data.issue_text,
+        created_by: data.created_by,
+        assigned_to: data.assigned_to,
+        status_text: data.status_text
+      })
+
       
     })
     
