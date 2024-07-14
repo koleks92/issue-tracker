3428541bc51f483400b688bd715d18f61581e768
koleks92 <koleks92@gmail.com>
2024-07-14 18:40:35 +0200

    feat: schema and model

---
+++ db_schema.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const IssueSchema = new Schema({
+    project: { type: String, required: true}, 
+    issue_title: { type: String, required: true },
+    issue_text: { type: String, required: true },
+    created_on: { type: Date, default: Date.now },
+    updated_on: { type: Date, default: Date.now },
+    created_by: { type: String, required: true },
+    assigned_to: { type: String },
+    open: { type: Boolean, default: true },
+    status_text: { type: String }
+  });
+
+const Issue = mongoose.model('Issue', IssueSchema);
+
+module.exports = Issue;
+
